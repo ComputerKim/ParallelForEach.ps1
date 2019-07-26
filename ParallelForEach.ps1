6d486a91d075c1ec09cc366a220a9f8fdd39db3d
ComputerKim <computerkim@live.dk>
2019-07-26 09:59:26 +0200

    Initial

---
+++ ParallelForEach.ps1
@@ -0,0 +1,11 @@
+﻿function ParallelForEach {
+    param([scriptblock]$Process, [parameter(ValueFromPipeline)]$InputObject)
+    $runspaces = $Input | ForEach {
+        $r = [PowerShell]::Create().AddScript("param(`$_);$Process").AddArgument($_)
+        [PSCustomObject]@{ Runspace = $r; Handle = $r.BeginInvoke() }
+    }
+    $runspaces | ForEach { 
+        while (!$_.Handle.IsCompleted) { sleep -m 100 }
+        $_.Runspace.EndInvoke($_.Handle)
+    }
+}
